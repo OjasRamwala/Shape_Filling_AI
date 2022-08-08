d96d2ada30875258b206785e4023b3162f497c0e
Ojas A Ramwala <ojasramwala@gmail.com>
2022-08-08 21:26:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxP
 from tensorflow.keras.models import Model
 
 def conv_block(inputs, num_filters):
+    
     x = Conv2D(num_filters, 3, padding = 'same')(inputs)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
@@ -14,12 +15,14 @@ def conv_block(inputs, num_filters):
     return x
 
 def encoder_block(inputs, num_filters):
+    
     x = conv_block(inputs, num_filters)
     p = MaxPool2D((2, 2))(x)
 
     return x, p
 
 def decoder_block(inputs, skip_features, num_filters):
+    
     x = Conv2DTranspose(num_filters, (2, 2), strides = 2, padding = 'same')(inputs)
     x = Concatenate()([x, skip_features])
     x = conv_block(x, num_filters)
@@ -27,6 +30,7 @@ def decoder_block(inputs, skip_features, num_filters):
     return x
 
 def unet(input_shape):
+    
     inputs = Input(input_shape)
     s1, p1 = encoder_block(inputs, 64)
     s2, p2 = encoder_block(p1, 128)
